b2c24a6516da43c0f822b8db3f64f60a8c3d8ff6
immanuelsavio <immanuelsavio@github.com>
2018-12-12 13:11:35 +0530

    Complete

--- analyse.py
+++ analyse.py
@@ -1,8 +1,12 @@
 #This file is used to analyse twitter feeds using textblob and classify into positive, negative or zero.
-import textblob
+from textblob import TextBlob
 import tweepy
 
+consumer_key = '<Enter Key>'
+consumer_secret = '<Enter Key>  '
 
+access_token = '<Enter Key>'
+access_token_secret = '<Enter Key>'
 
 authenticate = tweepy.OAuthHandler(consumer_key, consumer_secret)
 authenticate.set_access_token(access_token,access_token_secret)
@@ -12,3 +16,6 @@ search_tweets = api.search('KCR')
 
 for tweet in search_tweets:
     print(tweet.text)
+
+    analysis = TextBlob(tweet.text)
+    print(analysis.sentiment)
